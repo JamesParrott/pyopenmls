36ec4817a99826e58cfa3f999642c2ce3528a77c
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-05-29 21:20:52 +0100

    Update signature_key_pair.rs

--- src/api/signature_key_pair.rs
+++ src/api/signature_key_pair.rs
@@ -21,6 +21,7 @@ impl PySignatureKeyPair {
         let error_msg = format!("Could not generate signature scheme for code: {:?}",sig_scheme_code);
         let signature_scheme = SignatureScheme::try_from(sig_scheme_code).expect(&error_msg);
         Self {
+                // Currently only supports ECDSA_SECP256R1_SHA256 and ED25519
                 wrapped: SignatureKeyPair::new(signature_scheme).expect("Could not generate signature key pair"),
                 signature_scheme: signature_scheme,
             }
