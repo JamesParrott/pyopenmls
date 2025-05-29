18cc786a07ef4b17edc608182fe93e3734cd33c5
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-05-29 17:29:33 +0100

    Don't add sum_as_string to module

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ mod api;
 
 #[pymodule]
 fn pyopenmls(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(api::sum_as_string::sum_as_string, m)?)?;
     m.add_class::<api::basic_credential::PyBasicCredential>()?;
     m.add_class::<api::cipher_suite::PyCiphersuite>()?;
     m.add_class::<api::signature_scheme::PySignatureScheme>()?;
