b483b22f25bf77608d426de675c97b2e6b42c8b0
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-05-30 11:06:35 +0100

    Rename to openmls_rust_crypto_provider

---
+++ src/api/openmls_rust_crypto_provider.rs
@@ -0,0 +1,32 @@
+use std::default::{Default};
+
+use pyo3::prelude::{*};
+use openmls_rust_crypto::{OpenMlsRustCrypto};
+
+#[allow(dead_code)]
+#[pyclass(name="OpenMlsRustCrypto")]
+pub struct PyOpenMlsRustCrypto {
+    wrapped : OpenMlsRustCrypto,
+
+}
+
+#[pymethods]
+impl PyOpenMlsRustCrypto {
+
+    // Open MlsRustcrypto is just a simple Struct with derived Default trait. 
+    // Giving new args requires wrapping RustCrypto and MemoryStorage
+    // openmls_rust_crypto\src\lib.rs
+    // #[derive(Default, Debug)]
+    // #[cfg_attr(feature = "test-utils", derive(Clone))]
+    // pub struct OpenMlsRustCrypto {
+    //     crypto: RustCrypto,
+    //     key_store: MemoryStorage,
+    // }
+    #[new]
+    pub fn new() -> Self {
+        Self {
+            wrapped: Default::default(),
+        }
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ fn pyopenmls(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<api::basic_credential::PyBasicCredential>()?;
     m.add_class::<api::cipher_suite::PyCiphersuite>()?;
     m.add_class::<api::signature_scheme::PySignatureScheme>()?;
-    m.add_class::<api::openmls_rust_crypto::PyOpenMlsRustCrypto>()?;
+    m.add_class::<api::openmls_rust_crypto_provider::PyOpenMlsRustCrypto>()?;
     m.add_class::<api::signature_key_pair::PySignatureKeyPair>()?;
     Ok(())
 }
