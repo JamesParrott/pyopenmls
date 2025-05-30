09c4b8a118c9bca499714236e583266c5eea3da7
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-05-30 11:06:41 +0100

    Update smoke_test.py

--- smoke_test.py
+++ smoke_test.py
@@ -22,8 +22,8 @@ print_deets_of_enum_member(cipher_suite)
 print(f'{cipher_suite is Ciphersuite.MLS_256_XWING_CHACHA20POLY1305_SHA256_Ed25519=}')
 print(f'{cipher_suite.signature_algorithm()=}')
 
-provider = OpenMlsRustCrypto()
-print(f'{provider=}')
+default_provider = OpenMlsRustCrypto()
+print(f'{default_provider=}')
 
 # signature_key_pair_default = SignatureKeyPair()
 # print(f'{signature_key_pair_default=}')
@@ -35,6 +35,28 @@ signature_key_pair_ciphersuite = SignatureKeyPair(cipher_suite.signature_algorit
 print(f'{signature_key_pair_ciphersuite=}')
 
 
-# Reproduce quickstart in Python
+print('\n\n' 'Reproduce quickstart in Python')
 cipher_suite = Ciphersuite.MLS_128_DHKEMX25519_AES128GCM_SHA256_Ed25519
 provider = OpenMlsRustCrypto()
+
+def generate_credential_with_key(
+    identity: bytes,
+    signature_algorithm: SignatureScheme | None = None,
+    provider: OpenMlsRustCrypto = None,
+    cipher_suite: Ciphersuite | None = None,
+) -> tuple[None, SignatureKeyPair]:
+# ) -> tuple[CredentialWithKey, SignatureKeyPair]:
+    cipher_suite = cipher_suite or Ciphersuite.MLS_256_XWING_CHACHA20POLY1305_SHA256_Ed25519
+    signature_algorithm = signature_algorithm or cipher_suite.signature_algorithm()
+    provider = provider or OpenMlsRustCrypto()
+
+    basic_credential = BasicCredential(identity)
+    signature_key_pair = SignatureKeyPair(signature_algorithm)
+    
+
+    return (None, signature_key_pair)
+
+
+cred_w_key, key_pair = generate_credential_with_key(b'Super_secret_ID')
+
+print(f'{key_pair=}')
