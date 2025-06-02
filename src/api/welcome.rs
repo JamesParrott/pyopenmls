cf92f9eeb14d2cc0ef2acdbda3a9f6062b7ae6bf
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-06-02 17:24:23 +0100

    Remove empty impl block

--- src/api/welcome.rs
+++ src/api/welcome.rs
@@ -10,17 +10,4 @@ pub struct PyWelcome {
     pub wrapped: Welcome,
 }
 
-#[pymethods]
-impl PyWelcome {
-    // #[new]
-    // pub fn new(identity: Vec<u8>) -> Self {
-    //     Self {
-    //         wrapped: Welcome::new(identity),
-    //     }
-    // }
 
-    // #[getter]
-    // fn identity(&self) -> PyResult<&[u8]> {
-    //     Ok(self.wrapped.identity())
-    // }
-}
