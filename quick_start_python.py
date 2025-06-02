ace1260d489ac80abaf62d7eab2c57060a1c2461
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2025-06-02 13:40:11 +0100

    Update quick_start_python.py

--- quick_start_python.py
+++ quick_start_python.py
@@ -1,5 +1,3 @@
-import copy
-
 from pyopenmls import (BasicCredential,
                        CredentialType,
                        Ciphersuite,
@@ -29,6 +27,8 @@ def generate_credential_with_key(
     provider: OpenMlsRustCrypto = None,
     ciphersuite: Ciphersuite | None = None,
 ) -> tuple[CredentialWithKey, SignatureKeyPair]:
+
+    """ A helper to create credentials and key pairs. """
     basic_credential = BasicCredential(identity)
 
     ciphersuite = ciphersuite or Ciphersuite.MLS_128_DHKEMX25519_AES128GCM_SHA256_Ed25519
@@ -43,12 +43,10 @@ def generate_credential_with_key(
     # key_store = provider.storage()
     # signature_key_pair.store(key_store)
 
-    # print(f'{provider.storage_values=}')
     signature_key_pair.store_in_provider(provider)
-    # print(f'{provider.storage_values=}')
 
     public_key = signature_key_pair.public()
-    # print(f'{public_key=}')
+
     credential_with_key = CredentialWithKey(basic_credential, public_key)
 
     return (credential_with_key, signature_key_pair)
@@ -64,7 +62,6 @@ def generate_key_package(
 
     # Create the key package builder
     builder = KeyPackage.builder()
-    # print(f'{builder=}')
     
     # Create the key package bundle
     bundle = builder.build(
@@ -82,95 +79,85 @@ cred_w_key, key_pair = generate_credential_with_key(
     ciphersuite = ciphersuite,
     )
 
-# print(f'{cred_w_key=}')
-# print(f'{key_pair=}')
-
-key_package_bundle = generate_key_package(ciphersuite, provider, key_pair, cred_w_key)
-# print(f'{key_package_bundle=}')
+def main():
+    key_package_bundle = generate_key_package(ciphersuite, provider, key_pair, cred_w_key)
 
 
-#First they need credentials to identify them
-sasha_credential_with_key, sasha_signer = generate_credential_with_key(
-    b"Sasha",
-    CredentialType.Basic,
-    ciphersuite.signature_algorithm(),
-    provider,
-)
+    #First they need credentials to identify them
+    sasha_credential_with_key, sasha_signer = generate_credential_with_key(
+        b"Sasha",
+        CredentialType.Basic,
+        ciphersuite.signature_algorithm(),
+        provider,
+    )
 
-# Don't do this in real code.  See:
-# print(f'{provider.storage_values=}')
-# Maxim now has access to Sasha's private key (even more easily than they already did,
-# as they're both storing creds in the same Python process anyway). 
-maxim_credential_with_key, maxim_signer = generate_credential_with_key(
-    b"Maxim",
-    CredentialType.Basic,
-    ciphersuite.signature_algorithm(),
-    provider,
-)
+    # Don't do this in real code.  See:
+    # print(f'{provider.storage_values=}')
+    # Doing this grants Maxim access to Sasha's private key (even more easily than they already did,
+    # as they're both storing creds in the same Python process anyway). 
+    maxim_credential_with_key, maxim_signer = generate_credential_with_key(
+        b"Maxim",
+        CredentialType.Basic,
+        ciphersuite.signature_algorithm(),
+        provider,
+    )
 
-# print(f'{sasha_credential_with_key=}, {sasha_signer=}')
-# print(f'{maxim_credential_with_key=}, {maxim_signer=}')
-# Then they generate key packages to facilitate the asynchronous handshakes
-# in MLS
+    # Then they generate key packages to facilitate the asynchronous handshakes
+    # in MLS
 
-# Generate KeyPackages
-maxim_key_package = generate_key_package(ciphersuite, provider, maxim_signer, maxim_credential_with_key)
-
-# print(f'{maxim_key_package=}')
-
-# maxim_k_p_only = maxim_key_package.key_package()
-# print(f'{maxim_k_p_only=}')
-
-# Now Sasha starts a new group ...
-sasha_group = MlsGroup(
-    provider,
-    sasha_signer,
-    MlsGroupCreateConfig(),
-    sasha_credential_with_key,
-)
-# print(f'{sasha_group=}')
-
-# ... and invites Maxim.
-# The key package has to be retrieved from Maxim in some way. Most likely
-# via a server storing key packages for users.
-mls_message_out, welcome_out, group_info = (sasha_group
-    .add_member(provider, sasha_signer, maxim_key_package.key_package())
-)
-
-# Sasha merges the pending commit that adds Maxim.
-sasha_group.merge_pending_commit(provider)
+    # Generate KeyPackages
+    maxim_key_package = generate_key_package(ciphersuite, provider, maxim_signer, maxim_credential_with_key)
 
 
-# Sascha serializes the [`MlsMessageOut`] containing the [`Welcome`].
-serialized_welcome: bytes = welcome_out.tls_serialize_detached()
-# print(f'{serialized_welcome=}')
 
-# Maxim can now de-serialize the message as an [`MlsMessageIn`] ...
-mls_message_in = MlsMessageIn.tls_deserialize(serialized_welcome)
-# print(f'{mls_message_in=}')
+    # Now Sasha starts a new group ...
+    sasha_group = MlsGroup(
+        provider,
+        sasha_signer,
+        MlsGroupCreateConfig(),
+        sasha_credential_with_key,
+    )
 
-# ... and inspect the message.
-welcome = mls_message_in.extract_welcome()
-# print(f'{welcome=}')
-# welcome = match mls_message_in.extract() {
-#    MlsMessageBodyIn::Welcome(welcome) => welcome,
-#    # We know it's a welcome message, so we ignore all other cases.
-#    _ => unreachable!("Unexpected message type."),
-# };
-MlsGroupJoinConfig()
-# Now Maxim can build a staged join for the group in order to inspect the welcome
-maxim_staged_join = StagedWelcome.new_from_welcome(
-    provider,
-    MlsGroupJoinConfig(),
-    welcome,
-    # The public tree is need and transferred out of band.
-    # It is also possible to use the [`RatchetTreeExtension`]
-    sasha_group.export_ratchet_tree(),
-)
-# .expect("Error creating a staged join from Welcome");
+    # ... and invites Maxim.
+    # The key package has to be retrieved from Maxim in some way. Most likely
+    # via a server storing key packages for users.
+    mls_message_out, welcome_out, group_info = (sasha_group
+        .add_member(provider, sasha_signer, maxim_key_package.key_package())
+    )
 
-# Finally, Maxim can create the group
-maxim_group = maxim_staged_join.into_group(provider)
+    # Sasha merges the pending commit that adds Maxim.
+    sasha_group.merge_pending_commit(provider)
 
 
-# print(f'{maxim_group=}')
+    # Sascha serializes the [`MlsMessageOut`] containing the [`Welcome`].
+    serialized_welcome: bytes = welcome_out.tls_serialize_detached()
+
+    # Maxim can now de-serialize the message as an [`MlsMessageIn`] ...
+    mls_message_in = MlsMessageIn.tls_deserialize(serialized_welcome)
+
+    # ... and inspect the message.
+    welcome = mls_message_in.extract_welcome()
+    # welcome = match mls_message_in.extract() {
+    #    MlsMessageBodyIn::Welcome(welcome) => welcome,
+    #    # We know it's a welcome message, so we ignore all other cases.
+    #    _ => unreachable!("Unexpected message type."),
+    # };
+    MlsGroupJoinConfig()
+    # Now Maxim can build a staged join for the group in order to inspect the welcome
+    maxim_staged_join = StagedWelcome.new_from_welcome(
+        provider,
+        MlsGroupJoinConfig(),
+        welcome,
+        # The public tree is need and transferred out of band.
+        # It is also possible to use the [`RatchetTreeExtension`]
+        sasha_group.export_ratchet_tree(),
+    )
+
+    # Finally, Maxim can create the group
+    maxim_group = maxim_staged_join.into_group(provider)
+
+
+
+
+if __name__ == '__main__':
+    main()
